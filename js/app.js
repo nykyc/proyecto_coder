70f6e0ca7c13a0d2f5a5cba5709c21504f0968fb
Nico Anderson <nicoo_anderson@hotmail.com>
2022-09-20 16:36:11 -0300

    agregando unos detalles

--- js/app.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {  
-    var btn_menu = document.querySelector('.btn_menu') 
-    if (btn_menu) { 
-        btn_menu.addEventListener('click', () => { 
-            var menu_items = document.querySelector('#navegacion') 
-            navegacion.classList.toggle('show')
-        }) 
-    } 
-}
